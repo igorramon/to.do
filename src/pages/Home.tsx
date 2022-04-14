a54af472251afda1d217ad205ecb7c533a6cd62a
igorramon <igor-ramon@outlook.com>
2022-04-14 11:58:46 -0300

    challenge finalized

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,25 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data= {
+      id: new Date().getTime(),
+      title : newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const item = updatedTasks.find(task => task.id ===id );
+    if(!item)
+      return;
+    item.done = !item.done; 
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
